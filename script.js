d2e7e6e56190ac82a1ed4295b9e7a63a0b0f37fa
dpontonloor <139184607+dpontonloor@users.noreply.github.com>
2023-07-18 21:57:48 -0500

    Add files via upload
    Cambios totales

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function crearTablaUsuarios() {
       'CREATE TABLE IF NOT EXISTS usuarios (usrname VARCHAR(20) PRIMARY KEY, nombre VARCHAR(50), apellido VARCHAR(50), fecnacim DATE, celular VARCHAR(10), email VARCHAR(50), direccion VARCHAR(100), fpago VARCHAR(20), tarjeta VARCHAR(20), password VARCHAR(20), eslogin VARCHAR(1))',
       [],
       function () {
-        //window.alert('Tabla usuarios creada con éxito');
+        window.alert('Tabla usuarios creada con éxito');
         console.log('Tabla usuarios creada con éxito');
       },
       function (tx, error) {
@@ -65,7 +65,6 @@ function loginUsuario() {
         window.location.href = 'index.html';
       },
       function (tx, error) {
-        window.alert('Error al iniciar sesión:', error.message);
         console.log('Error al iniciar sesión:', error.message);
       }
     );
@@ -279,3 +278,111 @@ function generateRatingStars(rating) {
 window.addEventListener('DOMContentLoaded', function () {
   verificaLogin();
 });
+
+//variables
+let allContainerCart = document.querySelector('.products');
+let containerBuyCart = document.querySelector('.card-items');
+let priceTotal = document.querySelector('.price-total')
+let amountProduct = document.querySelector('.count-product');
+
+
+let buyThings = [];
+let totalCard = 0;
+let countProduct = 0;
+
+//functions
+loadEventListenrs();
+function loadEventListenrs(){
+    allContainerCart.addEventListener('click', addProduct);
+
+    containerBuyCart.addEventListener('click', deleteProduct);
+}
+
+function addProduct(e){
+    e.preventDefault();
+    if (e.target.classList.contains('btn-add-cart')) {
+        const selectProduct = e.target.parentElement; 
+        readTheContent(selectProduct);
+    }
+}
+
+function deleteProduct(e) {
+    if (e.target.classList.contains('delete-product')) {
+        const deleteId = e.target.getAttribute('data-id');
+
+        buyThings.forEach(value => {
+            if (value.id == deleteId) {
+                let priceReduce = parseFloat(value.price) * parseFloat(value.amount);
+                totalCard =  totalCard - priceReduce;
+                totalCard = totalCard.toFixed(2);
+            }
+        });
+        buyThings = buyThings.filter(product => product.id !== deleteId);
+        
+        countProduct--;
+    }
+    //FIX: El contador se quedaba con "1" aunque ubiera 0 productos
+    if (buyThings.length === 0) {
+        priceTotal.innerHTML = 0;
+        amountProduct.innerHTML = 0;
+    }
+    loadHtml();
+}
+
+function readTheContent(product){
+    const infoProduct = {
+        image: product.querySelector('div img').src,
+        title: product.querySelector('.title').textContent,
+        price: product.querySelector('div p span').textContent,
+        id: product.querySelector('a').getAttribute('data-id'),
+        amount: 1
+    }
+
+    totalCard = parseFloat(totalCard) + parseFloat(infoProduct.price);
+    totalCard = totalCard.toFixed(2);
+
+    const exist = buyThings.some(product => product.id === infoProduct.id);
+    if (exist) {
+        const pro = buyThings.map(product => {
+            if (product.id === infoProduct.id) {
+                product.amount++;
+                return product;
+            } else {
+                return product
+            }
+        });
+        buyThings = [...pro];
+    } else {
+        buyThings = [...buyThings, infoProduct]
+        countProduct++;
+    }
+    loadHtml();
+    //console.log(infoProduct);
+}
+
+function loadHtml(){
+    clearHtml();
+    buyThings.forEach(product => {
+        const {image, title, price, amount, id} = product;
+        const row = document.createElement('div');
+        row.classList.add('item');
+        row.innerHTML = `
+            <img src="${image}" alt="">
+            <div class="item-content">
+                <h5>${title}</h5>
+                <h5 class="cart-price">${price}$</h5>
+                <h6>Amount: ${amount}</h6>
+            </div>
+            <span class="delete-product" data-id="${id}">X</span>
+        `;
+
+        containerBuyCart.appendChild(row);
+
+        priceTotal.innerHTML = totalCard;
+
+        amountProduct.innerHTML = countProduct;
+    });
+}
+ function clearHtml(){
+    containerBuyCart.innerHTML = '';
+ }
